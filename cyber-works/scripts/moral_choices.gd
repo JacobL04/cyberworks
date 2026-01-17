0a848ea7a6c52785d69bb39994ef67a80b386f02
Ichen-ai <22ivanchen@gmail.com>
2026-01-17 00:14:19 -0500

    started moral choices

---
+++ cyber-works/scripts/moral_choices.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var button: Button = $Button
+@onready var button_2: Button = $Button2
+@onready var button_3: Button = $Button3
+@onready var button_4: Button = $Button4
+@onready var progress_bar: TextureProgressBar = $ProgressBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
