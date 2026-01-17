7f3a0ffec07c6023c33c5652db23a423f9b0be01
Jacob Lee <jlee3887@uwo.ca>
2026-01-17 15:32:46 -0500

    feat: intro scene transition

--- cyber-works/scripts/start.gd
+++ cyber-works/scripts/start.gd
@@ -23,3 +23,11 @@ func _on_settings_but_button_up() -> void:
 
 func _on_quit_but_button_up() -> void:
 	pass # Replace with function body.
+
+
+
+func _on_start_but_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/intro_cutscenes/scene1.tscn")
+
+func _on_quit_but_pressed() -> void:
+	get_tree().quit()
