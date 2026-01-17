b51248fc06108cb168206dcc4ba8a21d1619bea7
Ichen-ai <22ivanchen@gmail.com>
2026-01-17 01:25:12 -0500

    finished moral template

--- cyber-works/scripts/moral_choices.gd
+++ cyber-works/scripts/moral_choices.gd
@@ -1,16 +1,37 @@
 extends Node2D
 
+@onready var progress_bar: TextureProgressBar = $ProgressBar
 @onready var button: Button = $Button
 @onready var button_2: Button = $Button2
 @onready var button_3: Button = $Button3
 @onready var button_4: Button = $Button4
-@onready var progress_bar: TextureProgressBar = $ProgressBar
+@onready var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	progress_bar.hide()
+	button.hide()
+	button_2.hide()
+	button_3.hide()
+	button_4.hide()
+	label.hide()
+	
+	progress_bar.value = 100
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if Main.moralChoices == true:
+		progress_bar.value -= 1
+
+func _on_button_button_up() -> void:
+	Main.moral += 2
+
+func _on_button_2_button_up() -> void:
+	Main.moral += 1
+
+func _on_button_3_button_up() -> void:
+	Main.moral -= 1
+
+func _on_button_4_button_up() -> void:
+	Main.moral -= 2
