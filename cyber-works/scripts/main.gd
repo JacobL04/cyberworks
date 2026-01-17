b858b1a8f47d823123304d8f944e220fdf8dae5c
Ichen-ai <22ivanchen@gmail.com>
2026-01-17 18:53:43 -0500

    clean up code

--- cyber-works/scripts/main.gd
+++ cyber-works/scripts/main.gd
@@ -10,10 +10,6 @@ var techChallenge = false
 func _ready() -> void:
 	toStart()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
 #first moral choice
 func toMoralChoice():
 	get_tree().change_scene_to_file.call_deferred("res://scenes/moral_choices.tscn")
