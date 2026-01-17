e340d04893bfa1a66521416da32b5b5f66aa4db3
Ichen-ai <22ivanchen@gmail.com>
2026-01-17 12:46:28 -0500

    create script for main menu

---
+++ cyber-works/scripts/start.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var start_but: TextureButton = $StartBut
+@onready var settings_but: TextureButton = $SettingsBut
+@onready var quit_but: TextureButton = $QuitBut
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_start_but_button_up() -> void:
+	pass # Replace with function body.
+
+func _on_settings_but_button_up() -> void:
+	pass # Replace with function body.
+
+func _on_quit_but_button_up() -> void:
+	pass # Replace with function body.
