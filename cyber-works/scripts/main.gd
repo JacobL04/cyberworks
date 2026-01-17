1239b2f62efc447c4813429f753e62de8d507bcd
Ichen-ai <22ivanchen@gmail.com>
2026-01-17 14:24:59 -0500

    call defered change scene

--- cyber-works/scripts/main.gd
+++ cyber-works/scripts/main.gd
@@ -8,7 +8,7 @@ var techChallenge = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://scenes/start.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/start.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
