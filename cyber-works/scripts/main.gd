3fd953c3d83890f610483dc9ef4c9616943c2a02
Ichen-ai <22ivanchen@gmail.com>
2026-01-17 12:34:11 -0500

    basic start screen

--- cyber-works/scripts/main.gd
+++ cyber-works/scripts/main.gd
@@ -8,10 +8,13 @@ var techChallenge = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-	
+
+#first moral choice
+func toMoralChoice():
+	get_tree().change_scene_to_file("res://scenes/moral_choices.tscn")
